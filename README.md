5b66e104ea224fd10e21ec8a3ef889f74b924701
Srikkanth Ramesh <srikkanth@apple.com>
2018-08-18 22:52:33 -0700

    adding "no image to display"

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 ![](https://raw.githubusercontent.com/srikkanthsiki/zillowinterview/master/src/image/Screenshot%202018-08-18%2022.33.31.png)
 ![](https://raw.githubusercontent.com/srikkanthsiki/zillowinterview/master/src/image/Screenshot%202018-08-18%2022.33.36.png)
 ![](https://raw.githubusercontent.com/srikkanthsiki/zillowinterview/master/src/image/Screenshot%202018-08-18%2022.33.42.png)
+
+## if no image then it shows
+![](https://raw.githubusercontent.com/srikkanthsiki/zillowinterview/master/src/image/Screenshot%202018-08-18%2022.51.36.png)
--- src/component/photo.jsx
+++ src/component/photo.jsx
@@ -6,43 +6,7 @@ import Gallery from 'react-grid-gallery';
 import _ from 'lodash';
 
 const IMAGES =
-    [{
-        src: "https://wp.zillowstatic.com/1/162Acari-042-95d6b9.jpg",
-        thumbnail: "https://wp.zillowstatic.com/1/162Acari-042-95d6b9.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 174,
-        isSelected: true,
-        caption: "My Dream Home"
-    },
-        {
-            src: "https://wp.zillowstatic.com/1/David-Hasselhoffs-kitchen-62f58c.jpg",
-            thumbnail: "https://wp.zillowstatic.com/1/David-Hasselhoffs-kitchen-62f58c.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212,
-            caption: "My Kitchen"
-        },
-
-        {
-        src: "https://wp.zillowstatic.com/1/The-White-House-3228c3-640x427.jpg",
-        thumbnail: "https://wp.zillowstatic.com/1/The-White-House-3228c3-640x427.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 212,
-        caption: "White house"
-    },
-        {
-            src: "https://photos.zillowstatic.com/p_h/ISe87v7o8m569l1000000000.jpg",
-            thumbnail: "https://photos.zillowstatic.com/p_h/ISe87v7o8m569l1000000000.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212,
-            caption: "My friends house"
-        },
-        {
-            src: "https://photos.zillowstatic.com/p_f/ISq9kfp4m9eyco1000000000.jpg",
-            thumbnail: "https://photos.zillowstatic.com/p_f/ISq9kfp4m9eyco1000000000.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212,
-            caption: "brady's house"
-        }]
+    []
 
 
 
@@ -63,12 +27,11 @@ class Photo extends Component {
 
     render() {
 
-            if(IMAGES.size ==0){
+            if(IMAGES.length == 0){
                 return (  <div>No image to display</div>)
             }else {
                return( <div><Gallery images={IMAGES}/></div>)
             }
-
 
     }
 }
